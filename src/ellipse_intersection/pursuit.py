4f65c5a7209d43572ea7e7ae911a748fe05987c2
yunminseop <001208yun@gmail.com>
2025-02-03 10:28:53 +0900

    pure pursuit

--- src/ellipse_intersection/pursuit.py
+++ src/ellipse_intersection/pursuit.py
@@ -5,6 +5,9 @@ import torch
 import torch.nn as nn
 import logging
 import gc
+import matplotlib.pyplot as plt
+from matplotlib.patches import Polygon
+from scipy.special import binom
 
 # YOLO verbose False 적용
 logging.getLogger("ultralytics").setLevel(logging.ERROR)
@@ -30,7 +33,7 @@ class Conversion:
         self.__inch = inch
 
         self.__PPI = np.sqrt(np.power(self.__w_res, 2)+np.power(self.__h_res, 2))/self.__inch
-
+        
         self.x = 0
         self.y = 0
 
@@ -43,27 +46,28 @@ class VideoProcessor:
         self.model = model
 
         self.unit = Conversion(1920, 1080, 16.1)
-
-        self.ellipse_center = (320, 640)  # 타원의 중심
-        self.ellipse_axes = (320, 100)    # 타원의 반지름 (a, b)
-        
         self.center = Centroid()
-        self.slope = 0.0
+        self.car_position = np.array([320, 940])
+        self.lookahead_distance = 60
 
     def process_video(self):
         cap = cv2.VideoCapture(self.video_path)
-        standard_point = (320, 540)
+        
 
         if not cap.isOpened():
             print("Error: Couldn't open the video file.")
             exit()
 
+        plt.ion()
+        fig, ax = plt.subplots(figsize=(6,6))
+
+        ax.set_xlim(0, 640)
+        ax.set_ylim(1000, 0)
+
         fps = cap.get(cv2.CAP_PROP_FPS)
 
-        intersection_finder = GetIntersection(self.ellipse_center, self.ellipse_axes)
-
         while True:
-
+            
             ret, frame = cap.read()
             frame = cv2.rotate(frame, cv2.ROTATE_180)
             
@@ -75,91 +79,120 @@ class VideoProcessor:
             
             results = self.model.predict(frame_resized)
             
-            for result in results:
-                boxes = result.boxes.xyxy.cpu().numpy()
-                classes = result.boxes.cls.cpu().numpy()
-                """
-                cls_id = {0: center, 1: right, 2: left, 7: safety zone}
-                """
-                masks = result.masks.xy if result.masks else None
-                # print(classes)
-
-                if masks != None:
-                    prev_masks = masks
-                    for cls_id, mask in zip(classes, masks):
-                        if cls_id == 0:
-                            # print(f"Class: {model.names[int(cls_id)]}")
-                            self.center.get_centroid(mask)
-                elif prev_masks:
-                            for cls_id, mask in zip(classes, prev_masks):
-                                if cls_id == 0:
-                                    self.center.get_centroid(mask)
-
-            dynamic_point = (self.center.centroid_x, self.center.centroid_y)
-            intersection_finder.set_dynamic_line(dynamic_point)
-
-            annotated_frame = results[0].plot(boxes=False)
-
-            cv2.ellipse(annotated_frame, self.ellipse_center, self.ellipse_axes, 0, 0, 360, (150, 150, 150), 1)
-
-            cv2.circle(annotated_frame, standard_point, 5, (0, 0, 255), -1, cv2.LINE_AA)
-
-            points = intersection_finder.calculate_intersection()
-            
-            if points:
-                valid_points = intersection_finder.filter_valid_points(points, 640, 640)
-                for point in valid_points:
-                    cv2.circle(annotated_frame, point, 5, (0, 255, 0), -1, cv2.LINE_AA)
-                
-                    if (640-point[0])*(self.unit.p2cm()) < 320*self.unit.p2cm():
-                        self.x = -(320 - point[0])*self.unit.p2cm()
-                    else:
-                        self.x = (point[0] - 320)*self.unit.p2cm()
-
-                self.y = (640-point[1])*self.unit.p2cm()*7.5
-
-            self.slope = np.arctan(self.x/(0.396*self.y+6.3)) if point[1] != 0 else np.arctan(self.x)
+            for seg_result in results:
                     
-            self.slope = np.degrees(self.slope)
-            print(self.slope)
+                    classes = seg_result.boxes.cls.cpu().numpy() #cls_id = {0: center, 1: right, 2: left, 7: safety zone}
+                    masks = seg_result.masks.xy if seg_result.masks else None
+
+                    if masks != None:
+                        prev_masks = masks
+                        """ classes = [cls_id[0], cls_id[1], cls_id[2], ... ]
+                            masks = [mask[0], mask[1], mask[2], ... ]
+                                    mask[0] = [[20.2, 428.5], [167.0, 39.4], [420.7, 350.9], ...] (polygon)"""
+
+                        for cls_id, mask in zip(classes, masks):
+                            if cls_id == 0:
+                                    # print(f"Class: {self.model.names[int(cls_id)]}")
+                                    self.destination = mask
+                                
+                    elif prev_masks:
+                                for cls_id, mask in zip(classes, prev_masks):
+                                    if cls_id == 0:
+                                        self.destination = mask
+
+
+                    self.center.get_centroid(self.destination)                    
+
+                    annotated_frame = results[0].plot(boxes=False)
+                    
+            processor = PurePursuit(self.destination)
+            bezier_points = processor.get_bezier_points(self.car_position, (self.center.centroid_x, self.center.centroid_y))
+            bezier_path = processor.bezier_curve(bezier_points)
+            lookahead_point = processor.find_lookahead_point(bezier_path, self.car_position, self.lookahead_distance)
             
-            x, z = self.Calculate(self.slope)
+            ax.clear()
+            polygon = Polygon(self.destination, closed=True, edgecolor='r', facecolor='none', linewidth=1, label="Lane Edge")
+            ax.add_patch(polygon)
+            ax.plot(bezier_path[:, 0], bezier_path[:, 1], 'g-', label="Bezier Curve")
+            ax.scatter(*zip(*bezier_points), color='blue', label="Control Points")
+            ax.scatter(*lookahead_point, color='purple', s=100, label="Lookahead Point")
+            print(f"Lookahead Point: {lookahead_point}")                
 
-            # if z == 0.5784577935329023:
-            #     cv2.imwrite("gimochi4.jpg", frame_resized)
-            #     break
-
-            print(x, z)
-
+            ax.invert_yaxis()
+            ax.set_xlim(0, 640)
+            ax.set_ylim(1000, 0)
+            ax.set_aspect('equal')
+            ax.legend()
+            
+            plt.draw()
+            plt.pause(0.1)
             cv2.imshow('center_pursuit', annotated_frame)
             
-            delay = int(1000 / fps) # original fps
+            delay = int(50/ fps) # original fps
             
             del frame
             gc.collect()
 
-            if cv2.waitKey(delay) & 0xFF == ord('q'):
+            if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
+        cap.release()
+        cv2.destroyAllWindows()
+        plt.ioff()
+        plt.show()
 
-    def Calculate(self, slope):
-            x = 0.29
-            if -45.0 <= slope <= 0.0:
-                # -45.0~0.0 -> 0.65~0.79
-                source_min, source_max = -45.0, 0.0
-                target_min, target_max = 0.79, 0.65
-            elif 0.0 <= slope <= 45.0:
-                # 0.0~45.0 -> 0.56~0.77
-                source_min, source_max = 0.0, 45.0
-                target_min, target_max = 0.56, 0.77
-            else:
-                raise ValueError("slope out of range: must be between -45.0 and 45.0")
-            
-            # 정규화 공식 적용
-            z = target_min + (slope - source_min) * (target_max - target_min) / (source_max - source_min)
-                
-            return (x, z)
+class PurePursuit():
+    def __init__(self, lane_polygon):
+        self.lane_polygon = lane_polygon
+
+    def get_bezier_points(self, car_position, centroid):
+        
+        trans_polygon = self.lane_polygon.copy()
+        dest = self.find_nearest_value(trans_polygon[:, 0], centroid[0])
+        target_y = trans_polygon[trans_polygon[:, 0]==dest][0][1]
+
+        dist = centroid[1] - target_y
+
+        if dist > 0:
+            trans_polygon[:, 1] += int(dist)
+        else:
+            trans_polygon[:, 1] -= int(dist)
+
+        sort_index = np.argsort(trans_polygon[:, 1])
+        y_max = trans_polygon[sort_index[0]]
+
+        """ car_position ~ centroid"""
+        mid_control1 = (car_position[0]-(car_position[0] - centroid[0]) / 3, 1000 - ((car_position[1] - centroid[1]) * 5 / 10))
+        mid_control2 = (car_position[0]-(car_position[0] - centroid[0]) * 2 / 3, 1000- ((car_position[1] - centroid[1]) * 8 / 10))
+
+        """ centroid ~ y_max"""
+        mid_control3 = (centroid[0]-(centroid[0] - y_max[0]) / 3, centroid[1] - ((centroid[1] - y_max[1]) * 5 / 10))
+        mid_control4 = (centroid[0]-(centroid[0] - y_max[0]) * 2 / 3, centroid[1]- ((centroid[1] - y_max[1]) * 8 / 10))
+        
+        return (car_position, mid_control1, mid_control2, mid_control3, mid_control4, y_max)
+
+
+    def bezier_curve(self, bezier_points, num_points=100):
+        n = len(bezier_points) - 1
+        t_values = np.linspace(0, 1, num_points)
+        curve = np.zeros((num_points, 2))
     
+        for i in range(n + 1):
+            bernstein_poly = binom(n, i) * (t_values ** i) * ((1 - t_values) ** (n - i))
+            curve += np.outer(bernstein_poly, bezier_points[i])
+        
+        return curve
+
+    def find_lookahead_point(self, curve, current_pos, lookahead_distance):
+        distances = np.linalg.norm(curve - current_pos, axis=1)
+        idx = np.argmin(np.abs(distances - lookahead_distance))
+        return curve[idx]
+
+    def find_nearest_value(self, arr, value):
+        idx = np.argmin(np.abs(arr - value))
+        return arr[idx]
+
+
 class Centroid():
     def __init__(self):
         self.centroid_x, self.centroid_y = 0, 0
@@ -181,67 +214,9 @@ class Centroid():
             self.centroid_x /= (6 * area)
             self.centroid_y /= (6 * area)
 
-class GetIntersection:
-    def __init__(self, ellipse_center, ellipse_axes):
-        self.h, self.k = ellipse_center  
-        self.a, self.b = ellipse_axes   
-        self.fixed_point = (320, 640) # center of a frame
-
-    def set_dynamic_line(self, target_point):
-        x0, y0 = self.fixed_point
-        x1, y1 = target_point
-
-        # slope
-        self.m = (y1 - y0) / (x1 - x0) if x1 != x0 else float('inf')  # x1 == x0일 경우 수직선
-        
-        # y_intersection
-        self.c = y0 - self.m * x0
-
-
-    def calculate_intersection(self):
-        """get an intersection"""
-
-        if self.m == float('inf'):
-            return[[320, 540], [320, 740]]
-        
-        A = (1 / self.a**2) + (self.m**2 / self.b**2)
-        B = (2 * self.m * (self.c - self.k) / self.b**2) - (2 * self.h / self.a**2)
-        C = ((self.h**2) / self.a**2) + ((self.c - self.k)**2 / self.b**2) - 1
-        
-        # D = b^2 - 4ac
-        discriminant = B**2 - 4 * A * C
-
-        if discriminant < 0:
-            return None  # no intersection
-
-        x1 = (-B + np.sqrt(discriminant)) / (2 * A)
-        x2 = (-B - np.sqrt(discriminant)) / (2 * A)
-
-        y1 = self.m * x1 + self.c
-        y2 = self.m * x2 + self.c
-
-        if x1 > 0 and y1 > 0 or x1 < 0 and y1 > 0 :
-            self.upper_point = (x1, y1)
-        elif x2 > 0 and y2 > 0 or x2 < 0 and y2 > 0:
-            self.upper_point = (x2, y2)
-
-        return [[x1, y1], [x2, y2]]
-
-    def filter_valid_points(self, points, width, height):
-        """filtering only valid point 1, 2 quadrant"""
-        valid_points = [
-            (int(x), int(y)) for x, y in points
-            if 0 <= x <= width and 0 <= y <= height
-        ]
-        # print(f"valid_p: {valid_points}")
-        return valid_points
-
-
-
-
 
 if __name__ == "__main__":
-    video_path = '/home/ms/Downloads/video_output5.mp4'
+    video_path = '/home/ms/Downloads/video_output5.mp4' #'/home/ms/ws/git_ws/ComputerVision/src/ellipse_intersection/video_output6.mp4'
     checkpoint_path = '/home/ms/Downloads/best.pt'
 
     model = YOLO(checkpoint_path, verbose=False)
