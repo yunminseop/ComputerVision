67a8e81e6a2332456beb84d0c9db15f6b12bc9c7
yunminseop <001208yun@gmail.com>
2025-02-04 00:25:54 +0900

    detection model added and slope computing

--- src/ellipse_intersection/pursuit.py
+++ src/ellipse_intersection/pursuit.py
@@ -8,6 +8,7 @@ import gc
 import matplotlib.pyplot as plt
 from matplotlib.patches import Polygon
 from scipy.special import binom
+import math
 
 # YOLO verbose False 적용
 logging.getLogger("ultralytics").setLevel(logging.ERROR)
@@ -41,19 +42,24 @@ class Conversion:
         return  2.54 / self.__PPI
 
 class VideoProcessor:
-    def __init__(self, video_path, model):
+    def __init__(self, video_path, seg_model, det_model):
         self.video_path = video_path
-        self.model = model
+
+        self.seg_model = seg_model
+        self.det_model = det_model
 
         self.unit = Conversion(1920, 1080, 16.1)
         self.center = Centroid()
         self.car_position = np.array([320, 940])
         self.lookahead_distance = 60
 
+
+        self.frame_skip = 5  # 5배속
+        self.frame_count = 0
+
     def process_video(self):
         cap = cv2.VideoCapture(self.video_path)
         
-
         if not cap.isOpened():
             print("Error: Couldn't open the video file.")
             exit()
@@ -75,11 +81,25 @@ class VideoProcessor:
                 print("Error: Couldn't read a frame.")
                 break
             
+            self.frame_count += 1
+            if self.frame_count % self.frame_skip != 0:
+                continue
+
             frame_resized = cv2.resize(frame, (640, 640))
             
-            results = self.model.predict(frame_resized)
-            
-            for seg_result in results:
+            seg_results = self.seg_model.predict(frame_resized)
+            det_results = self.det_model.predict(frame_resized)
+
+            if det_results:
+                    for det_result in det_results:
+                    
+                        classes = det_result.boxes.cls.cpu().numpy()
+                        boxes = det_result.boxes.xyxy.cpu().numpy()  # (x_min, y_min, x_max, y_max)
+                        
+                        for cls_id in classes:
+                            print(f"Object: {self.det_model.names[int(cls_id)]}")
+
+            for seg_result in seg_results:
                     
                     classes = seg_result.boxes.cls.cpu().numpy() #cls_id = {0: center, 1: right, 2: left, 7: safety zone}
                     masks = seg_result.masks.xy if seg_result.masks else None
@@ -103,20 +123,22 @@ class VideoProcessor:
 
                     self.center.get_centroid(self.destination)                    
 
-                    annotated_frame = results[0].plot(boxes=False)
+                    annotated_frame = seg_results[0].plot(boxes=False)
                     
-            processor = PurePursuit(self.destination)
-            bezier_points = processor.get_bezier_points(self.car_position, (self.center.centroid_x, self.center.centroid_y))
-            bezier_path = processor.bezier_curve(bezier_points)
-            lookahead_point = processor.find_lookahead_point(bezier_path, self.car_position, self.lookahead_distance)
-            
+            processor = PurePursuit(self.destination, self.lookahead_distance)
+            lookahead_distance, self.bezier_points = processor.get_bezier_points(self.car_position, (self.center.centroid_x, self.center.centroid_y))
+            bezier_path = processor.bezier_curve(self.bezier_points)
+            lookahead_point = processor.find_lookahead_point(bezier_path, self.car_position, lookahead_distance)
+            slope = self.get_slope(lookahead_point)
+            print(f"slope: {slope:.3f} deg")
+
             ax.clear()
             polygon = Polygon(self.destination, closed=True, edgecolor='r', facecolor='none', linewidth=1, label="Lane Edge")
             ax.add_patch(polygon)
             ax.plot(bezier_path[:, 0], bezier_path[:, 1], 'g-', label="Bezier Curve")
-            ax.scatter(*zip(*bezier_points), color='blue', label="Control Points")
+            ax.scatter(*zip(*self.bezier_points), color='blue', label="Control Points")
             ax.scatter(*lookahead_point, color='purple', s=100, label="Lookahead Point")
-            print(f"Lookahead Point: {lookahead_point}")                
+            # print(f"Lookahead Point: {lookahead_point}")                
 
             ax.invert_yaxis()
             ax.set_xlim(0, 640)
@@ -141,9 +163,27 @@ class VideoProcessor:
         plt.ioff()
         plt.show()
 
+    def get_slope(self, lookahead_point):
+        x1, y1 = self.car_position
+        x2, y2 = lookahead_point
+
+        delta_y = y1 - y2 
+        delta_x = x1 - x2
+
+        desired_angle = math.atan2(delta_x, delta_y)
+
+        vehicle_angle = 0 
+
+        steering_angle = desired_angle - vehicle_angle
+
+        steering_angle = math.degrees((steering_angle + math.pi) % (2 * math.pi) - math.pi)
+
+        return steering_angle
+
 class PurePursuit():
-    def __init__(self, lane_polygon):
+    def __init__(self, lane_polygon, lookahead_distance):
         self.lane_polygon = lane_polygon
+        self.lookahead_distance = lookahead_distance
 
     def get_bezier_points(self, car_position, centroid):
         
@@ -158,18 +198,31 @@ class PurePursuit():
         else:
             trans_polygon[:, 1] -= int(dist)
 
+        if 0 <= np.abs(dist) <= 100:
+            """ dist가 100이면 lah_d+=50
+                dist가 50이면 lah_d+=100
+                즉, 무게중심과 edge간 거리가 가까워질수록 lah_d는 비례증가"""
+            self.lookahead_distance += (100 - np.abs(dist))
+        
+
         sort_index = np.argsort(trans_polygon[:, 1])
         y_max = trans_polygon[sort_index[0]]
 
-        """ car_position ~ centroid"""
-        mid_control1 = (car_position[0]-(car_position[0] - centroid[0]) / 3, 1000 - ((car_position[1] - centroid[1]) * 5 / 10))
-        mid_control2 = (car_position[0]-(car_position[0] - centroid[0]) * 2 / 3, 1000- ((car_position[1] - centroid[1]) * 8 / 10))
+        if dist < 100:
+            """ car_position ~ centroid"""
+            mid_control1 = (car_position[0]-(car_position[0] - centroid[0]) / 3, 1000 - ((car_position[1] - centroid[1]) * 5 / 10))
+            mid_control2 = (car_position[0]-(car_position[0] - centroid[0]) * 2 / 3, 1000- ((car_position[1] - centroid[1]) * 8 / 10))
 
-        """ centroid ~ y_max"""
-        mid_control3 = (centroid[0]-(centroid[0] - y_max[0]) / 3, centroid[1] - ((centroid[1] - y_max[1]) * 5 / 10))
-        mid_control4 = (centroid[0]-(centroid[0] - y_max[0]) * 2 / 3, centroid[1]- ((centroid[1] - y_max[1]) * 8 / 10))
-        
-        return (car_position, mid_control1, mid_control2, mid_control3, mid_control4, y_max)
+            """ centroid ~ y_max"""
+            mid_control3 = (centroid[0]-(centroid[0] - y_max[0]) / 3, centroid[1] - ((centroid[1] - y_max[1]) * 5 / 10))
+            mid_control4 = (centroid[0]-(centroid[0] - y_max[0]) * 2 / 3, centroid[1]- ((centroid[1] - y_max[1]) * 8 / 10))
+            
+            return (self.lookahead_distance, (car_position, mid_control1, mid_control2, mid_control3, mid_control4, y_max))
+        else:
+            """ car_position ~ centroid"""
+            mid_control1 = (car_position[0]-(car_position[0] - centroid[0]) / 3, 1000 - ((car_position[1] - centroid[1]) * 5 / 10))
+            mid_control2 = (car_position[0]-(car_position[0] - centroid[0]) * 2 / 3, 1000- ((car_position[1] - centroid[1]) * 8 / 10))
+            return (self.lookahead_distance, (car_position, mid_control1, mid_control2, centroid)) 
 
 
     def bezier_curve(self, bezier_points, num_points=100):
@@ -216,10 +269,12 @@ class Centroid():
 
 
 if __name__ == "__main__":
-    video_path = '/home/ms/Downloads/video_output5.mp4' #'/home/ms/ws/git_ws/ComputerVision/src/ellipse_intersection/video_output6.mp4'
-    checkpoint_path = '/home/ms/Downloads/best.pt'
+    video_path = './src/ellipse_intersection/video_output6.mp4' #'/home/ms/ws/git_ws/ComputerVision/src/ellipse_intersection/video_output6.mp4'
+    seg_path = '/home/ms/Downloads/best.pt'
+    det_path = '/home/ms/Downloads/best_det.pt'
 
-    model = YOLO(checkpoint_path, verbose=False)
+    seg_model = YOLO(seg_path, verbose=False)
+    det_model = YOLO(det_path, verbose=False)
 
-    processor = VideoProcessor(video_path, model)
+    processor = VideoProcessor(video_path, seg_model, det_model)
     processor.process_video()
